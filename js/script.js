b0c744ba01e989ef3b36e5a883ad0023333fb42c
JaquelineRabelo <jaqueline.raabelo@hotmail.com>
2023-09-15 16:42:32 +0100

    up15/09/23

--- js/script.js
+++ js/script.js
@@ -1,23 +1,6 @@
-let carrinho = []; // Array para armazenar os produtos no carrinho
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
 
-function addToCart(produto, preco) {
-  carrinho.push({ nome: produto, preco: preco });
-  alert(`"${produto}" foi adicionado ao carrinho.`);
-}
-
-function redirectToCart() {
-  // Salvar o carrinho no Local Storage
-  localStorage.setItem('carrinho', JSON.stringify(carrinho));
-
-  // Redirecionar para a página do carrinho
-  window.location.href = 'carrinho.html';
-}
-
-
-$(document).ready(function () {
-  // Quando o botão do menu hamburguer for clicado
-  $('.navbar-toggler').click(function () {
-    // Adicione a classe "open" ao elemento com a classe "fullscreen-menu"
-    $('.fullscreen-menu').toggleClass('open');
-  });
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
 });
