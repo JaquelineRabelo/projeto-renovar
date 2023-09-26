e3666edbee0186194f1316f674b9f032ff2fb313
JaquelineRabelo <jaqueline.raabelo@hotmail.com>
2023-09-26 18:56:44 +0100

    up map

--- js/script.js
+++ js/script.js
@@ -10,21 +10,14 @@ menuToggle.addEventListener('click', () => {
 
 
 /*adicionar ao carrinho*/
- // Variáveis para manter o carrinho de compras e o total
- const carrinho = [];
- let total = 0;
+const carrinhoLista = document.getElementById('carrinho-lista');
+const totalSpan = document.getElementById('total');
+let total = 0;
 
- // Função para adicionar um produto ao carrinho
- function adicionarAoCarrinho(nomeProduto, precoProduto) {
-     carrinho.push({ nome: nomeProduto, preco: precoProduto });
-     total += precoProduto;
-     atualizarCarrinho();
- }
-
- // Função para atualizar o valor total na div
- function atualizarCarrinho() {
-     const totalElement = document.getElementById("total");
-     totalElement.textContent = total.toFixed(2); // Formata o valor com duas casas decimais
- }
-
-
+function adicionarAoCarrinho(nome, preco) {
+    const listItem = document.createElement('li');
+    listItem.textContent = `${nome} - R$${preco.toFixed(2)}`;
+    carrinhoLista.appendChild(listItem);
+    total += preco;
+    totalSpan.textContent = `R$${total.toFixed(2)}`;
+}
