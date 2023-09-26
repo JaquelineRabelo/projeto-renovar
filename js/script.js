b4de2f6ccdf9a54e0fc516b8d3b13510199137cd
JaquelineRabelo <jaqueline.raabelo@hotmail.com>
2023-09-26 15:27:33 +0100

    up 26/09/2023

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+
+
+/*menu hamburgue*/
 const menuToggle = document.getElementById('menu-toggle');
 const navLinks = document.getElementById('nav-links');
 
@@ -6,7 +9,22 @@ menuToggle.addEventListener('click', () => {
 });
 
 
+/*adicionar ao carrinho*/
+ // Variáveis para manter o carrinho de compras e o total
+ const carrinho = [];
+ let total = 0;
 
+ // Função para adicionar um produto ao carrinho
+ function adicionarAoCarrinho(nomeProduto, precoProduto) {
+     carrinho.push({ nome: nomeProduto, preco: precoProduto });
+     total += precoProduto;
+     atualizarCarrinho();
+ }
 
+ // Função para atualizar o valor total na div
+ function atualizarCarrinho() {
+     const totalElement = document.getElementById("total");
+     totalElement.textContent = total.toFixed(2); // Formata o valor com duas casas decimais
+ }
 
 
